b5a7ac876336aec91977943a1b6f947da84abe28
GTaf <3484782+GTaf@users.noreply.github.com>
2021-05-20 23:25:11 +0200

    Last fixes for working POC

--- hummingbot/strategy/amm_arb/start.py
+++ hummingbot/strategy/amm_arb/start.py
@@ -2,17 +2,19 @@ from decimal import Decimal
 from hummingbot.strategy.market_trading_pair_tuple import MarketTradingPairTuple
 from hummingbot.strategy.amm_arb.amm_arb import AmmArbStrategy
 from hummingbot.strategy.amm_arb.amm_arb_config_map import amm_arb_config_map
+import ast
 
 
 def start(self):
     market_tuples = amm_arb_config_map.get("market_list").value
+    if isinstance(market_tuples, str):
+        market_tuples = ast.literal_eval(market_tuples)  # Loading from config file
     order_amount = amm_arb_config_map.get("order_amount").value
     min_profitability = amm_arb_config_map.get("min_profitability").value / Decimal("100")
     market_1_slippage_buffer = amm_arb_config_map.get("market_1_slippage_buffer").value / Decimal("100")
     market_2_slippage_buffer = amm_arb_config_map.get("market_2_slippage_buffer").value / Decimal("100")
     market_3_slippage_buffer = amm_arb_config_map.get("market_3_slippage_buffer").value / Decimal("100")
     concurrent_orders_submission = amm_arb_config_map.get("concurrent_orders_submission").value
-
     self._initialize_markets([(connector.lower(), [market]) for (connector, market) in market_tuples])
     bases = [market.split("-")[0] for (_, market) in market_tuples]
     quotes = [market.split("-")[1] for (_, market) in market_tuples]
